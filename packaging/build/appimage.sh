50c3be0a25b8406285142412d786868b3f66f4c3
Gergo Vari <github@gergovari.com>
2026-02-03 08:24:31 +0100

    Bump version to v0.16

--- packaging/build/appimage.sh
+++ packaging/build/appimage.sh
@@ -6,7 +6,7 @@ set -e
 
 # 1. Setup paths
 BASE_DIR=$(pwd)
-VERSION="v0.15" # Initial placeholder, will be bumped
+VERSION="v0.16" # Initial placeholder, will be bumped
 ARCH=$(uname -m)
 PACKAGING_DIR="$BASE_DIR/packaging"
 BUILD_TOOLS_DIR="$PACKAGING_DIR/build"
